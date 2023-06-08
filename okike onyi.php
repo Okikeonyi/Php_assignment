3fb36ecee160adcdc8525a9b34e0b2b351347b71
Okikeonyi <133234771+Okikeonyi@users.noreply.github.com>
2023-06-08 08:02:19 +0100

    Add files via upload

---
+++ okike onyi.php
@@ -0,0 +1,10 @@
+<?php
+$name = "OKIKE ONYINYECHI ANTHONIA";
+$regno = "2021/Hnd/33130/cs<br>";
+$state_of_origin = "EBONYI STATE<br>";
+$project = "<strong>Design and implementation of crime documentation system.</strong>";
+echo $name;
+echo $regno;
+echo $state_of_origin;
+echo $project;
+?>
